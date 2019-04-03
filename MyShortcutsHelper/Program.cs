26bd2c8c96ae88f4226290339e219acf19952750
Morihiro Nagano <morihiro-nagano@jslabo.com>
2019-04-03 11:48:26 +0900

    プロジェクト ファイルを追加します。

---
+++ MyShortcutsHelper/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyShortcutsHelper
+{
+    public static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+            //        if (System.Diagnostics.Process.GetProcessesByName(
+            //System.Diagnostics.Process.GetCurrentProcess().ProcessName).Length > 1)
+            //        {
+            //        }
+            //エラーハンドラを登録
+            Application.ThreadException += new System.Threading.ThreadExceptionEventHandler(Application_ThreadException);
+            Thread.GetDomain().UnhandledException += new UnhandledExceptionEventHandler(Program_UnhandledException);
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+
+        static void Program_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Exception ex = e.ExceptionObject as Exception;
+            if (ex != null)
+            {
+                ShowError(ex, "UnhandledException");
+            }
+        }
+
+        static void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
+        {
+            ShowError(e.Exception, "ThreadException");
+        }
+
+        public static void ShowError(Exception ex, string title)
+        {
+            MessageBox.Show("プログラム中で補足されなかったエラーが発生しました。詳細はエラーログをごらん下さい。", title);
+
+            StreamWriter stream = new StreamWriter("error.txt", true);
+            stream.WriteLine("[" + title + "]");
+            stream.WriteLine("[message]\r\n" + ex.Message);
+            stream.WriteLine("[source]\r\n" + ex.Source);
+            stream.WriteLine("[stacktrace]\r\n" + ex.StackTrace);
+            stream.WriteLine();
+            stream.Close();
+        }
+    }
+
+}
---
+++ MyShortcutsHelper/ShortcutData.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShortcutsHelper
+{
+    [Serializable]
+    public class ShortcutData
+    {
+        [DllImport("kernel32", EntryPoint = "GlobalAddAtomA")]
+        static extern short GlobalAddAtom(string lpString);
+
+        public string AppPath { get; set; }
+        public string AppName { get; set; }
+        public string AppKey { get; set; }
+        public short AppKeyID { get; set; }
+        public ShortcutData() { }
+        public ShortcutData(string OneLineData)
+        {
+            string[] splitData = OneLineData.Split(',');
+            if (splitData.Count() != 3)
+            {
+                AppName = "";
+                AppPath = "";
+                AppKey = "";
+                AppKeyID = 0;
+            }
+            else
+            {
+                AppPath = splitData[0];
+                AppName = splitData[1];
+                AppKey = splitData[2];
+                AppKeyID = GlobalAddAtom("GlobalHotKey " + AppKey);
+            }
+            return;
+        }
+        public ShortcutData(string AppPath, string AppName, string AppKey, short AppKeyID)
+        {
+            this.AppKey = AppKey;
+            this.AppPath = AppPath;
+            this.AppName = AppName;
+            this.AppKeyID = AppKeyID;
+        }
+    }
+}
